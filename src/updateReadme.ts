e1748a770617cf307ff4b0f17208c18e75274354
Gordon Mickel <gordon.mickel@gmail.com>
2023-05-18 08:24:48 +0200

    Refactor: Hopefully fix the dist folder output

--- src/updateReadme.ts
+++ src/updateReadme.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import { getProjectRoot } from './src/config/index.js';
+import { getProjectRoot } from './config/index.js';
 
 const projectRootDir = getProjectRoot();
 
